197cf564cee22340b3a8c68b86466f72841dac8f
Fortune <Fortunestephen720@gmail.com>
2026-06-09 18:27:05 +0100

    initial commit

--- src/apps/fan/pages/FanHome.tsx
+++ src/apps/fan/pages/FanHome.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useEvents } from '@/hooks/useEvents'
 import { formatPrice } from '@/lib/utils'
