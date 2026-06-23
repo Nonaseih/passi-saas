c244f30c1661e09c7403566c91588747da88696e
Fortune <Fortunestephen720@gmail.com>
2026-06-23 09:41:24 +0100

    initial commit

--- src/routes/StaffRoutes.tsx
+++ src/routes/StaffRoutes.tsx
@@ -3,6 +3,11 @@ import { ProtectedRoute } from '@/components/shared/ProtectedRoute'
 
 import { StaffLogin } from '@/apps/staff/pages/StaffLogin'
 import { StaffProgress } from '@/apps/staff/pages/StaffProgress'
+import { StaffSettings } from '@/apps/staff/pages/StaffSettings'
+import { StaffTicketTimeSettings } from '@/apps/staff/pages/StaffTicketTimeSettings'
+import { StaffNotificationSettings } from '@/apps/staff/pages/StaffNotificationSettings'
+import { StaffAccount } from '@/apps/staff/pages/StaffAccount'
+import { StaffHelp } from '@/apps/staff/pages/StaffHelp'
 import { StaffScanner } from '@/apps/staff/pages/StaffScanner'
 import { StaffHistory } from '@/apps/staff/pages/StaffHistory'
 
